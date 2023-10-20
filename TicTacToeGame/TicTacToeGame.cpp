594ef6e62d4c671766e0c077511cfcb7751bb847
Billy Karnchanapee <wkarnchanapee@gmail.com>
2023-10-20 18:07:37 +0100

    Fix png not loading

--- TicTacToeGame/TicTacToeGame.cpp
+++ TicTacToeGame/TicTacToeGame.cpp
@@ -1,18 +1,15 @@
-/*This source code copyrighted by Lazy Foo' Productions 2004-2023
-and may not be redistributed without written permission.*/
-
-//Using SDL, SDL_image, standard IO, and strings
-#include <SDL.h>
-#include <SDL_image.h>
-#include <stdio.h>
-#include <string>
 #include "GameSetup.h";
 
 int main(int argc, char* args[])
 {
 	GameSetup setupController;
 
-	if (!setupController.init() && !setupController.loadMedia())
+	if (!setupController.init())
+	{
+		return 1;
+	}
+
+	if (!setupController.loadMedia())
 	{
 		return 1;
 	}
