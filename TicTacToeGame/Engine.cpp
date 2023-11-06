e6ca86323db464d547689c1fcd4d7785c7b25daa
billy.karnchanapee <wkarnchanapee@gmail.com>
2023-11-06 00:14:36 +0000

    Limit Framerate

--- TicTacToeGame/Engine.cpp
+++ TicTacToeGame/Engine.cpp
@@ -3,8 +3,8 @@
 #include "ImageRenderer.h"
 #include "World.h"
 #include "TicTacToeGame.h"
-#include <ctime>
 #include <chrono>
+#include <cmath>
 
 int main(int argc, char* args[])
 {
@@ -15,7 +15,8 @@ int main(int argc, char* args[])
 
 	TicTacToeGame* gTicTacToeGame = new TicTacToeGame(2, gInputProcessor, gImageRenderer, gWorld);
 
-	int const MAX_FPS = 60;
+	int const MAX_FPS = 30;
+	double const MS_PER_FRAME = static_cast<double>(1000) / MAX_FPS;
 
 	//Main loop flag
 	bool gQuit = false;
@@ -28,8 +29,6 @@ int main(int argc, char* args[])
 	//While application is running
 	while (!gQuit)
 	{
-		//std::time_t timeAtStartOfUpdate = std::time(nullptr);
-		//const std::clock_t clockStart = std::clock();
 		auto startTime = std::chrono::high_resolution_clock::now();
 
 		//Process
@@ -49,14 +48,10 @@ int main(int argc, char* args[])
 		
 		gQuit = gInputProcessor->IsQuitPressed();
 
-		//std::time_t timeAtEndOfUpdate = std::time(nullptr);
-		//const std::clock_t clockEnd = std::clock();
-		//clock_t t = 1000 * (clockEnd - clockStart) /CLOCKS_PER_SEC;
 		auto endTime = std::chrono::high_resolution_clock::now();
 		const std::chrono::duration<double> delta = endTime - startTime;
-		double f = static_cast<double>(1000) / MAX_FPS;
-		double msToWait = f - delta.count();
-		SDL_Delay(f);
+		int msToWait = static_cast<int>(std::round(MS_PER_FRAME - delta.count()));
+		SDL_Delay(msToWait);
 	}
 
 	delete gSetupController;
