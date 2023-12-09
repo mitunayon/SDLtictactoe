6368c2384b9db4b84f702605c127f62f2b133fce
billy.karnchanapee <wkarnchanapee@gmail.com>
2023-12-09 20:47:49 +0000

    Add Failing Smoke Test

--- EngineTests/test.cpp
+++ EngineTests/test.cpp
@@ -1,6 +1,19 @@
 #include "pch.h"
 
+// Example Test
 TEST(TestCaseName, TestName) {
   EXPECT_EQ(1, 1);
   EXPECT_TRUE(true);
+}
+
+// Walking Skeleton
+TEST(WalkingSkeleton, SmokeTest)
+{
+	bool engineStarts = false;
+
+	// Start the game engine
+
+	// Load a game
+
+	EXPECT_TRUE(engineStarts);
 }
